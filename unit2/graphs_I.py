3d738b9e2de6e9e69d93846163babbe29037d0da
Alexander Sierra <AlexanderCSierra@alexanderrasmbp.lan>
2020-06-07 20:17:28 -0400

    finished lecture

--- unit2/graphs_I.py
+++ unit2/graphs_I.py
@@ -49,3 +49,102 @@ class Graph_Node:
     def __init__(self, value):
         self.value = value
         self.edges = [] #adjacency list
+
+#adjacency list could be a dictionary of connections for example:
+
+# {
+#     A: [B],
+#     B: [A, X, P, T]
+# }
+'''
+adjacency matrix is a 2d array containing info about all the connections
+
+adj list would be more relevant in a sparse graph, since we are only listing things that are connected. If we represented a sparse graph with a matrix we would have a lot of empty slots since it has to list all the nodes.
+
+adj matrix would be more relevant in a dense graph, since you can easily represent multiple connections without repeating so many times as in a list
+
+matrix also allows for additional info to be stored, perhaps edge weights instead of just 1s and 0s. 
+
+time to look up a connection would be O(n) in a list, O(1) in a matrix
+'''
+
+class Graph:
+    def __init__(self):
+        self.vertices = {}
+
+    def add_vertex(self, vertex_id):
+        self.vertices[vertex_id] = set() #set of edges
+
+    def add_edge(self, v1, v2):
+        if v1 in self.vertices and v2 in self.vertices:
+            self.vertices[v1].add(v2)
+        else:
+            raise IndexError("Vertex does not exist in graph")
+
+    def get_neighbors(self, vertex_id):
+        return self.vertices[vertex_id]
+
+    def bft(self, starting_vertex_id):
+        #breadth-first traversal
+        q = Queue()
+        q.enq(starting_vertex_id)
+
+        #keep track of visited nodes
+        visited = set()
+
+        #repeat until queue is empty
+        while q.size() > 0:
+
+            #deq the first vert
+            v=q.deq()
+
+            #if it's not visited:
+            if v not in visited:
+                print(v)
+                #mark as visited
+                visited.add(v)
+
+                for nex_vert in self.get_neightbors(v):
+                    q.enq(next_vert)
+
+g = Graph()
+
+g.add_vertex(99)
+g.add_vertex(3)
+g.add_vertex(3490)
+
+g.add_edge(99, 3)
+g.add_edge(99, 3490)
+g.add_edge(3, 99)
+
+print(g.get_neighbors(99))
+print(g.get_neighbors(3))
+
+
+'''
+Breadth-First Traversal
+
+add starting node to a queue
+(stack would allow for depth-first traversal)
+
+while queue isn't empty:
+    dequeue the first vert
+    if that vert isn't visited:
+        mark as visited
+        add all its unvisited neighbors to the queue
+
+'''
+
+'''
+Depth-First Traversal
+
+add starting node to a stack
+
+while stack isn't empty:
+    pop the first vert
+    if that vert isn't visited:
+        mark as visited
+        push all its unvisited neighbors to the stack
+'''
+
+
