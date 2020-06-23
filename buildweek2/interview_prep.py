4a0640028977a072d43741b91f4322e72619b1b9
Alexander Sierra <alexandercsierra@gmail.com>
2020-06-23 18:49:22 -0400

    day 2 interview prep

---
+++ buildweek2/interview_prep.py
@@ -0,0 +1,62 @@
+'''
+Describe the internet to someone who just got up from a 30 year coma?
+    -be more broad, less details
+    -getting into the weeds can get lost
+
+What are your salary requirements?
+    -salary range tailored to job
+    -if possible avoid exact numbers
+    -'a competitive salary for this area'
+    -more interested in the opportunity than the salary, I'm sure we can come to an agreement
+
+Tell me about a major setback you've had.
+    -make sure to end on a high point
+
+Why did you make the decision to switch careers?
+    -focus on personal growth, not money
+
+Tell me a little bit about yourself.
+    -elevator pitch
+        -tie in past skills and experiences
+
+Why should I hire you?
+    -focusing on not only doing the work but a can-do attitude
+    -why shouldn't you hire me?
+
+How do you deal with pressure or stressful situations?
+    -prioritize
+    -stopping to take a deep breath
+    -share a stressful situation
+'''
+
+'''
+Talk about a group project you were passionate about?
+*discuss how you worked on a team
+*talk about your individual contributions to that team
+
+Strengths/weaknesses as a developer
+*when you mention a weakness - tell how you are addressing this
+
+What is your dream job?
+*somewhere where growth is possible
+*ideally something acheivable within the company you're interviewing form
+*what skills do you want to use, what interests you, what are your values?
+
+Have you convinced a team to use a language they were not thrilled about, how did you do it?
+*test leadership skills
+    *show you came up with a democratic consensus
+*emphasis on the process of gaining consensus
+
+Give an example of a goal you reached, and tell me how you acheived it.
+*using STAR
+    *situation, task, action, result
+
+Have you ever made a mistake, how did you handle it?
+*provide a genuine mistake
+*talk about how you solved it specifically
+
+
+How would your coworkers describe me?
+*upsell the soft skills
+*avoid being arrogant or cheesy
+'''
