cf9ce50180b25be9cde65153c43e1cdecfdbe792
Alexander Sierra <alexandercsierra@gmail.com>
2020-06-23 14:08:26 -0400

    careers

--- buildweek2/bw_day_2.py
+++ buildweek2/bw_day_2.py
@@ -1,2 +1,105 @@
 #Leetcode
 #Intersection of two linked lists
+
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+​
+def getListLen(head: ListNode) -> int:
+    count = 0
+    
+    p = head
+    
+    while p is not None:
+        count += 1
+        p = p.next
+        
+    return count
+    
+class Solution:
+    def getIntersectionNode(self, headA: ListNode, headB: ListNode) -> ListNode:
+​
+        # Keep current pointers per list
+        pa = headA
+        pb = headB
+        
+        # Grab list lengths and compute the difference
+        len_a = getListLen(headA)
+        len_b = getListLen(headB)
+        
+        len_diff = len_a - len_b
+        
+        # Skip len_diff nodes in the longer list
+        if len_diff != 0:
+            nodes_to_skip = abs(len_diff)
+            
+            for _ in range(nodes_to_skip):
+                if len_diff > 0:  # list A is longer
+                    pa = pa.next
+                else:             # list B is longer
+                    pb = pb.next
+            
+        # Move pointers lockstep, see when they become the same
+        while pa is not None:
+            if pa is pb:
+                return pa
+            
+            pa = pa.next
+            pb = pb.next
+        
+        # If we get here, no intersection found
+        return None
+    
+        """
+        seen = set()
+        
+        p = headA
+        
+        # Add all list A nodes to a set
+        while p is not None:
+            seen.add(p)
+            p = p.next
+            
+        # Run through list B looking for seen nodes
+        p = headB
+        
+        while p is not None:
+            if p in seen:
+                return p
+            
+            p = p.next
+            
+        # If we got here, we didn't find an intersection
+        return None
+        """
+
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+​
+class Solution:
+    def hasCycle(self, head: ListNode) -> bool:
+        cur = head
+        visited = set()
+        
+        # Walk the entire linked list
+        while cur is not None:
+            
+            # Check to see if we found a loop
+            if cur in visited:
+                return True
+            
+            # Mark as visited and move to the next
+            visited.add(cur)
+            cur = cur.next
+            
+        # If we get here, no loop was detected
+        return False
+   
+
---
+++ buildweek2/bw_day_2_careers.py
@@ -0,0 +1,56 @@
+'''
+Submitting an application
+---------------------------
+*review the job description
+*highlight anything you have even a little experience in
+*make sure your resume reflects your experience
+*generally, higher things in the description are higher on the priority list for the employer
+*most relevant experience => cover letter
+
+
+Cardinal rule of interviewing
+--------------------------------
+Never ever under any circumstance answer a question with a "I don't know" unless it's followed by a "but..."
+*play curiosity
+    *show interest
+    *"oooo this is an interesting problem. I haven't seen that before let's see what we can do with this"
+
+
+Common Challenges/Concerns
+---------------------------
+BE/FE - is it ok to ignore the one that is worse?
+*acknowledge that one is a work on progress, and focus on better one
+
+I don't think final product is something I'm proud of
+*talk about a challenge you overcame
+*some aspect of it you are proud of
+
+None of my projects are done or deployed
+*share scope and timeframe
+*acknowledge next steps
+*show continued project
+
+I don't have any good challenges to talk about
+*everyone has challenges
+*also interpersonal challenges
+
+How do I highlight my contributions to a team project
+*highlight personal contributions
+*also highlight how you worked as part of a team
+
+Questions you don't have experience with
+*bring up the most related thing you do have experience with
+*I haven't spent a lot of time with that particular technology, but this is what I know about it. It's been on my list of things to research for a while
+*could also research after and mention in follow-up email
+
+Unfinished projects
+*update readme with future plans
+    *to do list
+    *make gradual updates with commit history
+*if there's something cool in there, talk about that
+
+Don't know the answer to a straightforward knowledge program
+"Pick the closest thing to the tech that I'd worked on. And admit that I'd not pursued that particular tech in detail. I'd list everything at all I did know about it.
+And add that it's on my list as something to research."
+
+'''
